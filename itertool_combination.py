5ef19f1ead8c4c062888e1170a3ec91639cd0cdc
VARAD SRIVASTAVA <varad.srivastava26@gmail.com>
2020-01-23 19:49:41 +0530

    Create itertool_combination.py

---
+++ itertool_combination.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 19:48:47 2020
+
+@author: Varad Srivastava
+
+This tool returns the  length subsequences of elements from the input iterable.
+
+Combinations are emitted in lexicographic sorted order. So, if the input iterable is sorted, the combination tuples will be produced in sorted order.
+
+Sample Code
+
+>>> from itertools import combinations
+>>> 
+>>> print list(combinations('12345',2))
+[('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]
+>>> 
+>>> A = [1,1,3,3,3]
+>>> print list(combinations(A,4))
+[(1, 1, 3, 3), (1, 1, 3, 3), (1, 1, 3, 3), (1, 3, 3, 3), (1, 3, 3, 3)]
+Task
+
+You are given a string .
+Your task is to print all possible combinations, up to size , of the string in lexicographic sorted order.
+
+Input Format
+
+A single line containing the string  and integer value  separated by a space.
+
+Constraints
+
+
+The string contains only UPPERCASE characters.
+
+Output Format
+
+Print the different combinations of string  on separate lines.
+
+Sample Input
+
+HACK 2
+Sample Output
+
+A
+C
+H
+K
+AC
+AH
+AK
+CH
+CK
+HK
+"""
+
+from itertools import combinations
+if __name__=="__main__":
+    S,k = input().split(" ")
+    M= [[]]*int(k)
+    K = int(k)
+    s = list(S)
+    s.sort()
+
+
+    for i in range(1,K+1):
+        M[i-1] = list(combinations(s,i))
+
+    for j in range(len(M)):
+        for z in range(len(M[j])):
+            print(*M[j][z],sep="")
+    
